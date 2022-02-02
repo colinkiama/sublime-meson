d2fe938a861ddbeef124917079f1ad72fe9bdff3
Colin Kiama <colinkiama@gmail.com>
2022-02-02 22:56:58 +0000

    Gathering data for async execution

--- setup.py
+++ setup.py
@@ -3,26 +3,32 @@ import os
 import subprocess
 from . import utils
 
-
 class MesonSetupInputHandler(sublime_plugin.TextInputHandler):
-	def name(self):
-		return "build_dir"
+    def name(self):
+        return "build_dir"
 
-	def placeholder(self):
-		return "Text to insert"
+    def placeholder(self):
+        return "Text to insert"
+
+    def validate(self, value):
+        return len(value.strip()) > 0           
 
 class MesonSetupCommand(sublime_plugin.WindowCommand):
     def run(self, build_dir):
-    	build_config_path = sublime_meson_utils.build_config_path(self)
-    	if build_config_path is None:
-    		return
+        self.build_dir = build_dir
+        self.build_config_path = utils.build_config_path(self)
+        if self.build_config_path is None:
+            return
 
-    	print("Build config file path:", str(build_config_path))
-    	print("build_dir:", build_dir)
+        print("Build config file path:", str(self.build_config_path))
+        print("build_dir:", build_dir)
 
-    	subprocess.run(['meson', 'setup', build_dir],
-    		cwd = sublime_meson_utils.project_folder(self))
+        subprocess.run(['meson', 'setup', build_dir],
+            cwd = utils.project_folder(self))
+
+    def __run_async(self):
+        print("I am async!")
 
     def input(self, args):
-    	if "build_dir" not in args:
-    		return MesonSetupInputHandler()
+        if "build_dir" not in args:
+            return MesonSetupInputHandler()
