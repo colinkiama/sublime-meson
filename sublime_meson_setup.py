495358d1eff695b8ebdbf272692fb59cb9183a04
Colin Kiama <colinkiama@gmail.com>
2022-02-02 02:05:55 +0000

    Detect meson.build file in current project directory

--- sublime_meson_setup.py
+++ sublime_meson_setup.py
@@ -9,9 +9,11 @@ class MesonSetupInputHandler(sublime_plugin.TextInputHandler):
 	def placeholder(self):
 		return "Text to insert"
 
+
 class MesonSetupCommand(sublime_plugin.TextCommand):
     def run(self, edit, build_dir):
-    	sublime_meson_utils.build_config_file(self)
+    	build_config_file = sublime_meson_utils.build_config_file(self)
+    	print("Build config file path:", build_config_file)
     	print("Line:", build_dir)
 
     def input(self, args):
--- sublime_meson_utils.py
+++ sublime_meson_utils.py
@@ -1,11 +1,14 @@
 import sublime
 import sublime_plugin
+import os
+
+BUILD_CONFIG_NAME = 'meson.build'
 
 def sublime_project_folder(self):
 	current_window = self.view.window()
 	folders = current_window.folders()
 	if len(folders) < 0:
-		return ""
+		return None
 
 	return folders[0]
 
@@ -13,9 +16,10 @@ def sublime_project_folder(self):
 def build_config_file(self):
 	project_folder_path = sublime_project_folder(self)
 	if len(project_folder_path) < 0 is False:
-		return ""
+		return None
 
-	print("Project folder path:", project_folder_path)
+	build_config_path = os.path.join(project_folder_path, BUILD_CONFIG_NAME)
+	if os.path.isfile(build_config_path) is False:
+		return None
 
-		
-
+	return build_config_path
